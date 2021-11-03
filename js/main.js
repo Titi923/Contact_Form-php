59b2fc1f4b2353cf8a119d2369af95d2b25eca81
Titi923 <petrisor.buciuta92@gmail.com>
2021-11-03 16:02:42 -0700

    Defining Flipping card.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+const card__flipping = document.querySelectorAll(".card__flipping");
+card__flipping.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("card__flipping-flipped");
+  })
+);
